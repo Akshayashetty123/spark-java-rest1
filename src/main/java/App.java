b64a50fd6a54ee9b51a358270d5af6cc1d05db36
Akshaya shetty <akshaya.shetty@ubona.com>
2023-08-18 15:43:03 +0530

    Third Push Println Statement Added in App Class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello folks");
         System.out.println("Second Push");
+        System.out.println("Third Push");
         HelloWorldEndpoint helloWorldEndpoint = new HelloWorldEndpoint();
         UserEndpoint userEndpoint = new UserEndpoint();
     }
