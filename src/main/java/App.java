cf0f57d6c8db5e2b601e26763b0ff9f82a1a91d0
Akshaya shetty <akshaya.shetty@ubona.com>
2023-08-18 15:38:30 +0530

    Second Push Println Statement Added in App Class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello folks");
+        System.out.println("Second Push");
         HelloWorldEndpoint helloWorldEndpoint = new HelloWorldEndpoint();
         UserEndpoint userEndpoint = new UserEndpoint();
     }
