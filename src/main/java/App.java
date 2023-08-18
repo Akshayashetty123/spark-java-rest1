625bde6fa1ca6b598ef9a57f5e5f532d0bdab410
Akshaya shetty <akshaya.shetty@ubona.com>
2023-08-18 15:53:37 +0530

    Fourth push added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,8 @@ public class App {
         System.out.println("Hello folks");
         System.out.println("Second Push");
         System.out.println("Third Push");
+        System.out.println("Fourth Push");
+
         HelloWorldEndpoint helloWorldEndpoint = new HelloWorldEndpoint();
         UserEndpoint userEndpoint = new UserEndpoint();
     }
