b10961b8ec20b849980371e9eee0232fc08baa00
Akshaya shetty <akshaya.shetty@ubona.com>
2023-08-18 16:40:32 +0530

    sixth push

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,7 @@ public class App {
         System.out.println("Second Push");
         System.out.println("Third Push");
         System.out.println("Fouth Push Skipped Its Fifth Push");
-
+        System.out.println("Sixth Push Added");
 
         HelloWorldEndpoint helloWorldEndpoint = new HelloWorldEndpoint();
         UserEndpoint userEndpoint = new UserEndpoint();
